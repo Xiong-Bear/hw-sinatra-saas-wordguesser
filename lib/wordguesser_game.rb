64f6f1ab464be5176eb1a20f20c9df403b1fc1c9
Xiong-Bear <31072312280@qq.com>
2021-10-20 15:33:35 +0800

    20211020-part1 solved

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,5 +1,7 @@
 class WordGuesserGame
 
+  attr_accessor :word, :guesses, :wrong_guesses, :valid, :word_with_guesses
+
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
@@ -7,7 +9,56 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-' * word.length
+    @cnt = 0
   end
+
+  def guess(letter)
+    if letter.nil? || letter == ''
+      @valid = false
+      raise ArgumentError
+    elsif (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')
+      letter = letter.downcase
+      if @word.include? letter
+        if @guesses.include? letter
+          @valid = false
+        else
+          @guesses += letter
+          @word.each_char.with_index do |c, i|
+            # puts @word_with_guesses[i]
+            @word_with_guesses[i] = letter unless c != letter
+          end
+          @valid = true
+          @cnt += 1
+        end
+      else
+        if @wrong_guesses.include? letter
+          @valid = false
+        else
+          @wrong_guesses += letter
+          @valid = true
+          @cnt += 1
+
+        end
+      end
+    else
+      @valid = false
+      raise ArgumentError
+    end
+  end
+
+  def check_win_or_lose
+    if @cnt <= 7 && @word_with_guesses == @word
+      :win
+    elsif @cnt == 7 && @word_with_guesses != @word
+      :lose
+    else
+      :play
+    end
+  end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
